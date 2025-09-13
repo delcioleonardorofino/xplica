99cafd326f94390a9bf3824e2012b97d42de31f2
delcioleonardorofino <delcioleonard@gmail.com>
2025-09-13 16:02:50 +0200

    Created models for simulados
    Created Universidade, Disciplina and Exame models.

--- core/exams/models.py
+++ core/exams/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Universidade(Model.models):
+  nome = models.CharField(max_length=70)
+
+class Disciplina(Model.models):
+  nome = models.CharField(max_length=70)
+  id_universidade = models.ForeignKey(Universidade, on_delete=models.CASCADE)
+  
+class Exame(Model.models):
+  ano = models.IntegerField()
+  id_disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
+  
